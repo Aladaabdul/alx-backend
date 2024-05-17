51047796938632309bec5e418c70a28658cd00e1
aladaabdul <aladarahman18@gmail.com>
2024-05-18 00:27:04 +0100

    0-simple_helper_function.py

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+"""index_range function"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """function that returns
+    start index and end index
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
