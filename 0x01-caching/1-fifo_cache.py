e605e11912dc39cda7faa6dd1623e110673fa423
aladaabdul <aladarahman18@gmail.com>
2024-05-21 18:32:53 +0100

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -20,7 +20,8 @@ class FIFOCache(BaseCaching):
 
         self.cache_data[key] = item
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS and \
+                key not in self.cache_data:
             first_key = next(iter(self.cache_data))
             print(f"DISCARD: {first_key}")
             del self.cache_data[first_key]
