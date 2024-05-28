ff457694f5e709b7fb2d6bf885821e1eda0e1a0d
aladaabdul <aladarahman18@gmail.com>
2024-05-28 17:35:02 +0100

    done

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+
+"""setup a basic Flask app"""
+
+
+from flask import render_template, request
+from flask_babel import Babel
+from flask import Flask
+
+
+class Config:
+    """Config class
+
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_locale():
+    """get_locale function
+
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def hello():
+    """return html template
+
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
+    babel.init_app(app, locale_selector=get_locale)
