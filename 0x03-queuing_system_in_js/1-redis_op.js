8702ec4093403322b4fb7ac846b1f4aa333d1e2b
aladaabdul <aladarahman18@gmail.com>
2024-06-24 20:50:12 +0100

    1-redis_op.js

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,32 @@
+// 1-redis_op.js
+
+import redis from "redis";
+const redisclient = redis.createClient();
+
+
+(async () => {
+	await redisclient.connect;
+})();
+
+redisclient.on("ready", () => {
+	console.log("Redis client connected to the server");
+});
+
+redisclient.on("error", (err) => {
+	console.log("Redis client not connected to the server:", err)
+});
+
+function setNewSchool(schoolName, value) {
+	redisclient.set(schoolName, value, redis.print);
+};
+
+function displaySchoolValue(schoolName) {
+	redisclient.get(schoolName, (err, value) => {
+		if (err) console.log(err);
+		else console.log(value);
+	})
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
